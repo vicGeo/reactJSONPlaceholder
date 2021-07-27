a1b7778b13e81ae5d07ed811f2aae7da68ec8f2c
vicGeo <vicsanig@alumni.upv.es>
2021-07-27 18:49:32 +0200

    testing render user per page

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const URL = "https://jsonplaceholder.typicode.com/users";
-export const USER_PER_PAGE = 5;
+export const USER_PER_PAGE = 4;
 export const TABLE_TITLES = ["ID", "NAME", "USERNAME", "EMAIL"];
